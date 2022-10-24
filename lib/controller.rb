543b25bbf6dfba369ae665f5d4cd11f9d86c7205
widmaker666 <114477330+widmaker666@users.noreply.github.com>
2022-10-24 16:41:36 +0200

    Add files via upload

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,35 +1,24 @@
+require 'gossip'
 require 'bundler'
 Bundler.require
 
 $:.unshift File.expand_path("./../lib", __FILE__)
-require 'gossip'
+
 
 class ApplicationController < Sinatra::Base
+    get '/' do 
+        erb :index 
+    end
 
-  get '/' do
-    erb :index, locals: {gossips: Gossip.all}
-  end
-
-  get '/gossips/new/' do
-    erb :new_gossip
-  end
-
-  post '/gossips/new/' do
-    Gossip.new("#{params['author']}","#{params['content']}").save
-    redirect'/'
-  end
-
-  get '/gossips/:id' do
-    erb :show, locals: {gossip: Gossip.find(params['id'].to_i), id:params['id']}
-  end
-
-  get '/gossips/:id/edit/' do
-    erb :edit, locals: {id: "#{params['id']}"}
-  end
-
-  post '/gossips/:id/edit/' do
-    Gossip.update("#{params['new_author']}","#{params['new_content']}", "#{params['updated_id']}")
-    redirect'/'
-  end
+    get '/gossips/new/' do
+        erb :new_gossip
+    end
+    
+    post '/gossips/new/' do
+        Gossip.new(params["gossip_author"], params["gossip_content"]).save
+        redirect '/'
+    end
 
 end
+
+
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -9,48 +9,8 @@ class Gossip
   end
 
   def save
-    CSV.open("./db/gossip.csv", "a") do |csv|
+    CSV.open("../Sinatra/db/gossip.csv", "ab") do |csv|
       csv << [@author, @content]
     end
   end
-
-  def self.all
-    all_gossips = []
-
-    CSV.read("./db/gossip.csv").each do |csv_line|
-      all_gossips << Gossip.new(csv_line[0], csv_line[1])
-    end
-    return all_gossips
-  end
-
-  def self.find(id)
-    gossips = []
-    CSV.read("./db/gossip.csv").each do |gossip|
-      gossips << Gossip.new(gossip[0], gossip[1])
-    end
-    gossip = gossips[id]
-    return gossip
-  end
-
-  def self.update(new_author, new_content, updated_id)
-    CSV.open("./db/gossip_update.csv",'w+') do |csv_update|
-      CSV.read("./db/gossip.csv").each_with_index do |gossip, index|
-        if index == updated_id.to_i
-          updated_gossip = Gossip.new(new_author, new_content)
-          csv_update <<[updated_gossip.author, updated_gossip.content]
-        else
-          csv_update << gossip
-        end
-      end
-    end
-    CSV.open("./db/gossip.csv",'w') do |old_row|
-      CSV.read("./db/gossip_update.csv").each do |new_row|
-        old_row << new_row
-      end
-    end
-  end
-
-  # def self.update(new_author, new_content, id)
-  #   table = CSV.table("./db/gossip.csv")
-  # end
 end
